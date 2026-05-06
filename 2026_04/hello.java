d28d5740a53dd391f116299ec1ff1bf5092c0d3e
HelloPong <77256939+Pong0882@users.noreply.github.com>
2026-05-06 23:10:57 +0900

    Change value of N from 10 to 11

--- 2026_04/hello.java
+++ 2026_04/hello.java
@@ -2,7 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        int N = 10;
+        int N = 11;
         for (int i = 0; i < N; i++) {
             for (int j = i; j < N; j++) {
                 System.out.print("*");
